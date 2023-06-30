2b025705666291b85125d55d71ac188576ec423a
Ulya Nafisatussaniyah <ulyanafisa0@gmail.com>
2023-06-30 17:03:43 +0700

    produk

--- src/views/KeranjangView.vue
+++ src/views/KeranjangView.vue
@@ -1,82 +1,82 @@
 <template>
   <div>
-        <div class="container">
-          <div class="row mt-4">
-            <div class="col">
-              <Navbar />
-              <h2>Daftar menu</h2>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col">
+          <Navbar />
+          <h2><strong>Daftar</strong> menu</h2>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <div class="col">
+          <div class="input-grup mb-3">
+            <input
+              v-model="search"
+              type="text"
+              class="form-control"
+              placeholder="Cari Skincare kamu.."
+              aria-label="Cari"
+              aria-describedby="basic-addon1"
+              @keyup="searchSkin"
+            />
+            <div class="input-grup-preopend">
+              <span class="input-grup-text" id="basic-addon1"> </span>
             </div>
           </div>
-
-          <div class="row mt-3">
-            <div class="col">
-              <div class="input-grup mb-3">
-
-                <input 
-                v-model="search"
-                type="text"
-                class="form-control"
-                placeholder="Cari Skincare kamu.."
-                aria-label="Cari"
-                aria-describedby="basic-addon1"
-                @keyup="searchSkin" />
-                <div class="input-grup-preopend">
-                  <span class="input-grup-text" id="basic-addon1">
-                    <b-icon-search></b-icon-search>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row mb-4">
-    <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
-      <Card  :product="product"/>
-  </div>
-  <Card/>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div
+          class="col-md-4 mt-4"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <Card :product="product" />
+        </div>
+        <Card />
+      </div>
     </div>
   </div>
-</div>
-
 </template>
 
 <script>
-  // @ is an alias to /src
-  import Navbar from "@/components/Navbar.vue";
-  import Card from "@/components/Card.vue";
-  import axios from 'axios';
-  
-  export default {
-    name: 'KeranjangView',
-    components: {
-      Navbar,
-        Card
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Card from "@/components/Card.vue";
+import axios from "axios";
+
+export default {
+  name: "KeranjangView",
+  components: {
+    Navbar,
+    Card,
+  },
+  data() {
+    return {
+      products: [],
+      search: "",
+    };
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
     },
-    data() {
-      return {
-        products: [],
-        search: '',
-      }
-    },
-    methods: {
-      setProducts(data) {
-        this.products = data;
-      },
-      searchSkin(){
-        axios
+    searchSkin() {
+      axios
         .get("http://localhost:3000/products?q=" + this.search)
         .then((response) => this.setProducts(response.data))
-        .catch((error) => console.log(error))
-      }
+        .catch((error) => console.log(error));
     },
-    mounted() {
-      axios
-        .get("http://localhost:3000/products")
-        .then((response) => this.setProducts(response.data))
-        .catch((error) => console.log(error))
-    }
-  }
-  </script>
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
 
 <style>
-
 </style>
