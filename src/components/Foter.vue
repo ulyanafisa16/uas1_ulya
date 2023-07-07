58a21c9dead71d6c7c4ddda50c182558832284ef
Ulya Nafisatussaniyah <ulyanafisa0@gmail.com>
2023-07-07 20:07:17 +0700

    footer

--- src/components/Foter.vue
+++ src/components/Foter.vue
@@ -5,15 +5,15 @@
     <div class="container">
         <div class="row">
             <div class="col text-left">
-              Copyright &copy; 2023 by Ulyanaf | All Rights Reserved.
+              Copyright &copy; 2023 by Originote | All Rights Reserved.
             </div>
             <div class="coltext-right">
             <div class="social-media" >
                     
-                    <a href="https://twitter.com/nafisaa_yaa"><b-icon-shopee></b-icon-shopee></a>
-                    <a href="https://instagram.com/ulyanafisatus"><b-icon-instagram></b-icon-instagram></a>
-                    <a href="https://linkedin.com/in/ulya-nafis"><b-icon-tokopedia></b-icon-tokopedia></a>
-                    <a href="https://github.com/ulyanafisa16"><b-icon-github></b-icon-github></a>
+                    <a href="https://wa.me/6287713108921"><b-icon-whatsapp></b-icon-whatsapp></a>
+                    <a href="https://instagram.com/theoriginote"><b-icon-instagram></b-icon-instagram></a>
+                    <a href="https://twitter.com/theoriginote"><b-icon-twitter></b-icon-twitter></a>
+                    <a href="https://facebook.com/theoriginote"><b-icon-facebook></b-icon-facebook></a>
                     <a href="https://codingawalannew.blogspot.com"><b-icon-blockquote-right></b-icon-blockquote-right></a>
                  </div>
                  </div>
